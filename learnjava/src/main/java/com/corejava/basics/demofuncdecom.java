3a7b313dd94c6233ffa148e4aba2f13ddc81b6de
Sabyasachi Mitra <sabyasachihyd2003@gmail.com>
2026-01-22 21:35:31 +0000

    new hava structure

---
+++ learnjava/src/main/java/com/corejava/basics/demofuncdecom.java
@@ -0,0 +1,5 @@
+public class demofuncdecom {
+    public static void main (String[] arg) {
+
+    }   
+}
---
+++ learnjava/src/main/java/com/corejava/basics/dummyTest.java
@@ -0,0 +1,29 @@
+public class dummyTest {
+    public static boolean chkLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }        
+     public static void main (String[] arg) {
+
+        for (int i = 1900; i <=2026; i++) {
+
+            if (chkLeapYear(i)) {
+                System.out.printf("%d is a Leap year\n", i);
+            }
+            // else {System.out.printf("%d is not a Leap Year\n", i);}
+
+        }
+
+        
+
+     }
+}
